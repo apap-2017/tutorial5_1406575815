896451cef04f48df691c2b6dd3b8ffd2537f658b
Mahdi Firdaus <pipipapipuu@gmail.com>
2017-10-04 19:19:37 +0700

    init repo tutorial5

---
+++ src/main/java/com/mahdifr/psp/PspTutorial5Application.java
@@ -0,0 +1,12 @@
+package com.mahdifr.psp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PspTutorial5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PspTutorial5Application.class, args);
+	}
+}
